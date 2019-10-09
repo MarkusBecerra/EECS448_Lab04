14ce4d722e91a735788c6551269706189f45b6fb
Markus Becerra <markusbecerra@ku.edu>
2019-10-09 00:39:47 -0500

    edited css file

--- Exercise3/customerBackend.php
+++ Exercise3/customerBackend.php
@@ -26,6 +26,7 @@ echo "<h3> Thank you for your order! </h3>";
 echo "<br> <h4> User Information: </h4>";
 echo "Username:" . $username . "<br>";
 echo "Password:" . $password . "<br>";
+
 echo "<table style = 'width: 100%; border: 1px solid black; text-align: center; margin: 0px auto;'> ";
 echo "<tr>";
 echo "<td> </td>";
@@ -34,8 +35,39 @@ echo "<th> Cost Per Item </th>";
 echo "<th> Sub Total </th>";
 echo "</tr>";
 
+echo "<tr>";
+echo "<th> Madeon - Adventure </th>";
+echo "<td>" . $madeon . "</td>";
+echo "<td> $10 </td>";
+echo "<td> $" . $madeon * 10 . "</td>";
+echo"</tr>";
 
+echo "<tr>";
+echo "<th> Daft Punk - RAM </th>";
+echo "<td>" . $daftpunk . "</td>";
+echo "<td> $7 </td>";
+echo "<td> $" . $daftpunk * 7 . "</td>";
+echo"</tr>";
 
+echo "<tr>";
+echo "<th> Frank Ocean - Channel Orange </th>";
+echo "<td>" . $frankocean . "</td>";
+echo "<td> $12 </td>";
+echo "<td> $" . $frankocean * 12 . "</td>";
+echo"</tr>";
+
+echo "<tr>";
+echo "<th> Shipping </th>";
+echo "<td colspan = 2 >" . $shipping . "</td>";
+echo "<td>" . $shippingcost "</td>";
+echo"</tr>";
+
+echo "<tr>";
+echo "<th colspan = 3> Total Cost </th>";
+echo "<th> $" . $shippingcost + $frankocean * 12 +  $daftpunk * 7 +  $madeon * 10 "</th>";
+echo"</tr>";
+
+echo "</table>";
 
 
 
